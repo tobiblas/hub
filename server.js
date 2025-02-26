1657d677815ae2268b085ed7fbf2aa1814253ede
Tobias Jönsson <tobias.jonsson@netset.com>
2025-02-26 15:02:43 +0100

    Embryo for thermostat fronend

--- server.js
+++ server.js
@@ -14,29 +14,28 @@ app.set("views", path.join(__dirname, "views")); // Folder for templates
 
 // Endpoint to get data for a specific tab
 app.get("/data/:tab", async (req, res, next) => {
-    try {
-        const tabName = req.params.tab;
-        const filePath = path.join(__dirname, tabName, "data.json");
-
+    async function renderPoolPump(res) {
+        const filePath = path.join(__dirname, "poolpump", "data.json");
         const data = await fs.promises.readFile(filePath, "utf8");
         const jsonData = JSON.parse(data);
+        res.render("poolpump", {jsonData});
+    }
 
-        /*let htmlContent = `<h2>${jsonData.title}</h2>`;
-        htmlContent += `<p>${jsonData.description}</p>`;
-        htmlContent += `<ul>`;
-        jsonData.properties.forEach(dataset => {
-            htmlContent += `<li><strong>${dataset.name}</strong>: ${dataset.value}</li>`;
-        });
-        htmlContent += `</ul>`;
-        console.log(htmlContent);
-        res.send(htmlContent);*/
-        /*const jsonData = {
-            title: "My Page",
-            description: "This is a dynamically generated page.",
-            datasets: [{ name: "Temperature", value: "22°C" }, { name: "Humidity", value: "60%" }]
-        };*/
+    async function renderThermostat(res) {
+        const filePath = path.join(__dirname, "poolpump", "data.json");
+        const data = await fs.promises.readFile(filePath, "utf8");
+        const jsonData = JSON.parse(data);
+        res.render("poolpump", {jsonData});
+    }
 
-        res.render("poolpump", { jsonData });
+    try {
+        const tabName = req.params.tab;
+
+        if (tabName === "thermostat") {
+            await renderThermostat(res);
+        } else if (tabName === "poolpump") {
+            await renderPoolPump(res);
+        }
     } catch (err) {
         next(err); // Pass the error to the global error handler
     }
